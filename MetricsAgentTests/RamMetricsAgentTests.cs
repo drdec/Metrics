2bd788cab4db80579f56c4ced1313ee88d824c96
DrDeckto <fynjy138@gmail.com>
2022-05-09 21:54:10 +0300

    дз

---
+++ MetricsAgentTests/RamMetricsAgentTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace MetricsAgentTests
+{
+    public class RamMetricsAgentTests
+    {
+        private RamMetricsController _ramMetricsController;
+
+        public RamMetricsAgentTests()
+        {
+            _ramMetricsController = new RamMetricsController();
+        }
+
+        public void GetDotNetMetricsFromAgent_ReturnOk()
+        {
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            IActionResult result = _ramMetricsController.GetRamMetrics();
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
